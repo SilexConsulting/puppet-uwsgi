a336e41c7837c0a2b171daf0ceb6dea190ab7220
David Otton <david@otton.org>
2014-10-16 12:55:00 +0100

    Move Python outside uwsgi

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,16 +1,4 @@
 class uwsgi::install {
-  if ! defined(Package["python-dev"]) {
-    package { "python-dev":
-      ensure => present,
-    }
-  }
-
-  if ! defined(Package["python-pip"]) {
-    package { "python-pip":
-      ensure => present,
-    }
-  }
-
   if ! defined(Package["uwsgi"]) {
     package { "uwsgi":
       ensure   => present,
